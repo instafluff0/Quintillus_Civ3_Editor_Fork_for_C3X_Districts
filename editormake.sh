ba62817d8783dd692c819d7e199987bc6759c20c
Insta Fluff <instafluff0@gmail.com>
2025-10-28 13:46:53 -0700

    Add bash script

---
+++ editormake.sh
@@ -0,0 +1,23 @@
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/liberica-jdk-21-full.jdk/Contents/Home"
+export PATH="$JAVA_HOME/bin:$PATH"
+
+cd ./Civ3_Shared_Components
+mvn clean install -DskipTests
+cd ../Civ3_Editor
+mvn clean install -DskipTests
+cp  ./src/main/java/com/civfanatics/civ3/xplatformeditor/langs ./target/langs
+#mvn clean install -DskipTests
+#mvn assembly:assembly
+cd target
+mkdir dist
+cd dist
+mkdir imgs
+mkdir bin
+mkdir help
+cp ../../imgs/*.PNG ./imgs/
+cp ../../imgs/spectrum.pcx ./imgs/
+cp ../../help/*.html ./help/
+cp ../../bin/BIQDecompressor.jar ./bin
+cp -r ../langs .
+mv ../s.jar "Conquests Editor.jar"
+cd ../../..
